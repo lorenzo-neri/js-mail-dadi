8c2a686a9a5487e0185b27d9c32ce1dbadd65029
lorenzo-neri <lorenzo21neri@gmail.com>
2023-08-28 15:59:35 +0200

    completed dice game in console

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,4 +6,21 @@ Prima di partire a scrivere codice poniamoci qualche domanda: Che ci sia un arra
 - Mail
 Chiedi all'utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
 Bonus:
-Usiamo un input e un bottone per inserire la mail e poi mostriamo i risultati in pagina. */
+Usiamo un input e un bottone per inserire la mail e poi mostriamo i risultati in pagina. */ 
+
+
+//GIOCO DEI DADI
+//Generare un numero random da 1 a 6 per il giocatore
+const playerDice = Math.floor((Math.random() * 6) + 1);
+console.log(`Dado del giocatore = ${playerDice}`);
+//Generare un numero random da 1 a 6 per il computer
+const cpuDice = Math.floor((Math.random() * 6) + 1);
+console.log(`Dado del computer = ${cpuDice}`);
+//Stabilire il vincitore col punteggio più alto
+if(playerDice > cpuDice){
+    console.log('Hai vinto!');
+} else if (playerDice === cpuDice) {
+    console.log('Pareggio!');
+} else {
+    console.log('Hai perso!');
+}
